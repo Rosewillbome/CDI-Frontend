45553fc27b5af170936b327ac3131df9594abd35
Rosewillbome <rosewillbometech@gmail.com>
2025-03-16 19:30:52 +0300

    changes

--- app/(static-maps)/static-maps/page.js
+++ app/(static-maps)/static-maps/page.js
@@ -134,7 +134,7 @@ function Page() {
             {Data?.length > 0 ? (
               <>
                 {endYear - startYear === 0 ? (
-                  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                  <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6">
                     {months.map((month) => (
                       <div
                         key={`${startYear}-${month[0]}`}
