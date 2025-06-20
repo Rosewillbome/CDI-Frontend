fd1395aa2b3f69fc44e1ae35e206f77ec1e65f66
Davidwekesaa <dweke12@gmail.com>
2025-06-20 17:58:05 +0300

    butch processing

--- app/components/ui/statics/StaticModal.jsx
+++ app/components/ui/statics/StaticModal.jsx
@@ -7,28 +7,31 @@ import Modal from "@mui/material/Modal";
 import { Menu } from "@headlessui/react";
 import { FileText, X } from "lucide-react";
 import { Download, Loader } from "lucide-react";
-import DownloadStaticFiles from "./DownloadStaticFiles";
 import { IconButton } from "@mui/material";
+import axios from "axios";
+import { refactorStaticMapData } from "../../../utils/selectYear";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  height: "90%",
-  width: "90%",
+  height: "20%",
+  width: "40%",
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
-  overflowY: "scroll",
-  overflowX: "scroll",
+  overflowY: "hidden",
+  overflowX: "hidden",
 };
 
 function StaticModal({ data, endYear, startYear, selectedIndicator }) {
   const [open, setOpen] = React.useState(false);
   // const [downloadOption, setDownloadOption] = useState("filtered");
   const [selectedOption, setSelectedOption] = useState("");
+  const [downloading, setDownloading] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const menuOpen = Boolean(anchorEl);
 
@@ -39,6 +42,49 @@ function StaticModal({ data, endYear, startYear, selectedIndicator }) {
     setSelectedOption(slctd);
     setOpen(true);
   };
+
+  const { yrs, groupedData } = refactorStaticMapData(
+    data,
+    startYear,
+    endYear,
+    selectedOption
+  );
+  const handleDownload = async () => {
+    setDownloading(true);
+    try {
+      const response = await axios.post(
+        "https://cdi-reports-generator.vercel.app/pdfs",
+        {
+          years: yrs,
+          groupedData: data,
+          selectedIndicator: selectedIndicator,
+        },
+        {
+          responseType: "blob", // Important for receiving binary data
+        }
+      );
+      setDownloading(false);
+      handleClose();
+      // Create download link
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "drought_monitoring_report.pdf");
+      document.body.appendChild(link);
+      link.click();
+
+      // Clean up
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.log("Error downloading PDF:", error.message);
+      // Add user feedback here (e.g., toast notification)
+    } finally {
+      setDownloading(false);
+      handleClose();
+    }
+  };
+
   return (
     <>
       <div className="flex items-center justify-center">
@@ -50,11 +96,11 @@ function StaticModal({ data, endYear, startYear, selectedIndicator }) {
             </div>
           </Menu.Button>
           <Menu.Items className="absolute right-0 m-2 w-40 bg-white border rounded-lg shadow-lg z-50">
-            <Menu.Item>
+            <Menu.Item className="mb-1">
               {({ active }) => (
                 <button
                   onClick={(e) => handleOpenModal(e, "selected_option")}
-                  className={`w-full text-left px-1 text-sm ${
+                  className={`w-full text-left m-1 text-sm ${
                     active ? "bg-gray-200" : ""
                   }`}
                 >
@@ -66,7 +112,7 @@ function StaticModal({ data, endYear, startYear, selectedIndicator }) {
               {({ active }) => (
                 <button
                   onClick={(e) => handleOpenModal(e, "all_data")}
-                  className={`w-full text-left px-1 text-sm ${
+                  className={`w-full text-left m-1 text-sm ${
                     active ? "bg-gray-200" : ""
                   }`}
                 >
@@ -85,26 +131,27 @@ function StaticModal({ data, endYear, startYear, selectedIndicator }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <IconButton
-            aria-label="close"
-            onClick={handleClose}
-            sx={{
-              position: 'absolute',
-              right: 8,
-              top: 8,
-              color: (theme) => theme.palette.grey[500],
-            }}
-          >
-            <X className="text-red-600" size={20} />
-          </IconButton>
-          <DownloadStaticFiles
-            data={data}
-            startYear={startYear}
-            endYear={endYear}
-            selectedIndicator={selectedIndicator}
-            // selectedOption={downloadOption === "filtered" ? "selected_option" : "all_data"}
-            selectedOption={selectedOption}
-          />
+          {downloading ? (
+            <div className=" w-full flex items-center justify-center">
+              <CircularProgress />
+              <p className="ml-2">{`Please Wait as we prepare ${
+                selectedIndicator?.trim()?.toLowerCase() === "vdi"
+                  ? "NDVI Anomaly"
+                  : selectedIndicator
+              } Report for (${
+                yrs?.length === 1
+                  ? `${yrs[0]}`
+                  : `${yrs[0]}-${yrs[yrs?.length - 1]}`
+              })`}</p>
+            </div>
+          ) : (
+            <button
+              className="flex items-center gap-2 p-2 bg-[#308DE0] text-white rounded-lg"
+              onClick={handleDownload}
+            >
+              <Download size={18} /> <span>Download the selected option</span>
+            </button>
+          )}
         </Box>
       </Modal>
     </>
